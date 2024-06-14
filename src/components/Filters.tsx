9abd217a6d9cf4f9b5b2c90e31de6718739bb892
architg-op <archit.g1998@gmail.com>
2024-06-14 19:13:41 +0530

    Filters display added, functionality to be added

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,66 +1,46 @@
 import { useState, React, useContext } from 'react';
 import { MyContext } from '../MyContext.tsx';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Checkbox from '@mui/material/Checkbox';
-import IconButton from '@mui/material/IconButton';
-import CommentIcon from '@mui/icons-material/Comment';
+import { Checkbox } from '@mui/material/Checkbox';
+
+import CheckboxList from './CheckboxList';
+import Typography from '@mui/material/Typography';
 
 export default function Filters() {
   const { articles, setArticles } = useContext(MyContext);
   //   console.log('articles ', articles);
-  const [checked, setChecked] = useState([0]);
+  function eliminateDuplicates(numbers) {
+    const sortedArray = [];
 
-  const handleToggle = (value: number) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    setChecked(newChecked);
-  };
+    numbers.forEach(function (element, index) {
+      if (numbers.indexOf(element) === index) {
+        sortedArray.push(element);
+      }
+    });
+    return sortedArray;
+  }
+  let categories = articles.map((article) => article.source);
+  let authors = articles.map((article) => article.author);
+  let sortby = ['Date', 'Title'];
+  categories = eliminateDuplicates(categories);
+  authors = eliminateDuplicates(authors);
+  sortby = eliminateDuplicates(sortby);
+  console.log('categories ', categories);
+  console.log('authors ', authors);
 
   return (
-    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-      {[0, 1, 2, 3].map((value) => {
-        const labelId = `checkbox-list-label-${value}`;
-
-        return (
-          <ListItem
-            key={value}
-            // secondaryAction={
-            //   <IconButton edge="end" aria-label="comments">
-            //     <CommentIcon />
-            //   </IconButton>
-            // }
-            disablePadding
-          >
-            <ListItemButton
-              role={undefined}
-              onClick={handleToggle(value)}
-              dense
-            >
-              <ListItemIcon>
-                <Checkbox
-                  edge="start"
-                  checked={checked.indexOf(value) !== -1}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ 'aria-labelledby': labelId }}
-                />
-              </ListItemIcon>
-              <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
-            </ListItemButton>
-          </ListItem>
-        );
-      })}
-    </List>
+    <>
+      <Typography variant="h6" gutterBottom>
+        Category
+      </Typography>
+      <CheckboxList data={categories} />
+      <Typography variant="h6" gutterBottom>
+        Author
+      </Typography>
+      <CheckboxList data={authors} />
+      <Typography variant="h6" gutterBottom>
+        Sort By
+      </Typography>
+      <CheckboxList data={sortby} />
+    </>
   );
 }
