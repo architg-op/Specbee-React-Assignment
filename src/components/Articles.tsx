a66b05cdb5d8b6bff9b57cd6006b143e2567bf6c
architg-op <archit.g1998@gmail.com>
2024-06-17 16:54:49 +0530

    Improvements in Articles Display

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -8,19 +8,11 @@ function Articles() {
     useContext(MyContext);
   console.log('filtersChecked ', filtersChecked);
   let filteredArticles = articles;
-  // filteredArticles.sort(function (b, a) {
-  //   var textA = a.date.toUpperCase();
-  //   var textB = b.date.toUpperCase();
-  //   return textA < textB ? -1 : textA > textB ? 1 : 0;
-  // });
-
-  // filtersChecked.length && filtersChecked[filtersChecked.length - 1] === 'Date'
-  //   ? filteredArticles.sort(function (a, b) {
-  //       var textA = a.date.toUpperCase();
-  //       var textB = b.date.toUpperCase();
-  //       return textA < textB ? -1 : textA > textB ? 1 : 0;
-  //     })
-  //   : null;
+  filteredArticles.sort(function (b, a) {
+    const textA = a.date.toUpperCase();
+    const textB = b.date.toUpperCase();
+    return textA < textB ? -1 : textA > textB ? 1 : 0;
+  });
 
   if (categoriesChecked.length) {
     filteredArticles = filteredArticles.filter((article) =>
@@ -34,45 +26,56 @@ function Articles() {
     );
   }
 
-  // filtersChecked.length && filtersChecked[filtersChecked.length - 1] === 'Title'
-  //   ? articles.sort(function (a, b) {
-  //       var textA = a.title.toUpperCase();
-  //       var textB = b.title.toUpperCase();
-  //       return textA < textB ? -1 : textA > textB ? 1 : 0;
-  //     })
-  //   : articles.sort(function (b, a) {
-  //       var textA = a.title.toUpperCase();
-  //       var textB = b.title.toUpperCase();
-  //       return textA < textB ? -1 : textA > textB ? 1 : 0;
-  //     });
+  filtersChecked.length && filtersChecked[filtersChecked.length - 1] === 'Date'
+    ? filteredArticles.sort(function (a, b) {
+        const dateA = new Date(a.date);
+        const dateB = new Date(b.date);
+        return dateA < dateB ? -1 : dateA > dateB ? 1 : 0;
+      })
+    : null;
+
+  filtersChecked.length && filtersChecked[filtersChecked.length - 1] === 'Title'
+    ? filteredArticles.sort(function (a, b) {
+        const textA = a.title.toUpperCase();
+        const textB = b.title.toUpperCase();
+        return textA < textB ? -1 : textA > textB ? 1 : 0;
+      })
+    : filteredArticles.sort(function (b, a) {
+        const textA = a.title.toUpperCase();
+        const textB = b.title.toUpperCase();
+        return textA < textB ? -1 : textA > textB ? 1 : 0;
+      });
 
   // console.log('filtersChecked', filtersChecked);
-
-  // console.log(articles);
 
   console.log('filtered articles length', filteredArticles.length);
 
   return (
     <>
-      {filteredArticles?.map((article) => {
-        return (
-          <Article
-            date={article.date}
-            title={article.title}
-            body={article.body}
-            image={article.image}
-            source={article.source}
-            author={article.author}
-          />
-        );
-      })}
-
-      <Pagination
-        variant="outlined"
-        color="primary"
-        count={10}
-        sx={{ paddingLeft: 110, paddingTop: 5, paddingBottom: 5 }}
-      />
+      {filteredArticles.length ? (
+        filteredArticles?.map((article) => {
+          return (
+            <Article
+              date={article.date}
+              title={article.title}
+              body={article.body}
+              image={article.image}
+              source={article.source}
+              author={article.author}
+            />
+          );
+        })
+      ) : (
+        <div
+          style={{
+            textAlign: 'center',
+            verticalAlign: 'middle',
+            lineHeight: '40',
+          }}
+        >
+          No result found for Selection
+        </div>
+      )}
     </>
   );
 }
